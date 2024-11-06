8dda1b82e54d2e797a744f2df29e024fb2143d44
fholmqvist <fredrik.d.holmqvist@gmail.com>
2024-11-06 10:23:15 +0100

    fleshed out main, updated gitignore, added Makefile

---
+++ Makefile
@@ -0,0 +1,28 @@
+SHELL := /bin/bash
+
+BUILD-TARGET = /usr/local/bin
+COVER = cover.out
+HTML  = cover.html
+
+.PHONY: build run install test cover cover-html clean
+
+run:
+	go run . --debug
+
+install:
+	go build -o rem .
+	sudo mv rem $(BUILD-TARGET)
+
+test:
+	go test ./...
+
+clean:
+	rm -rf $(COVER) $(HTML) out
+
+cover:
+	go test ./... -coverprofile=$(COVER)
+	go tool cover -func $(COVER) | grep total: | awk '{print $$3}'
+
+cover-html: clean cover
+	go tool cover -html=$(COVER) -o $(HTML)
+	xdg-open $(HTML)
--- main.go
+++ main.go
@@ -1,3 +1,56 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+
+	e "github.com/fholmqvist/remlisp/err"
+	h "github.com/fholmqvist/remlisp/highlight"
+	"github.com/fholmqvist/remlisp/lexer"
+	tk "github.com/fholmqvist/remlisp/token"
+)
+
+func main() {
+	bb, err := os.ReadFile("input.rem")
+	if err != nil {
+		panic(err)
+	}
+	lexer, err := lexer.New(bb)
+	if err != nil {
+		exit("error instantiating lexer", err)
+	}
+	tokens, erre := lexer.Lex()
+	if erre != nil {
+		exite("lexing error", bb, erre)
+	}
+	prettyPrintTokens(tokens)
+}
+
+func prettyPrintTokens(tokens []tk.Token) {
+	fmt.Printf("\n%s\n", h.Bold("TOKENS ============="))
+	if len(tokens) > 0 {
+		for i, t := range tokens {
+			num := fmt.Sprintf("%.4d", i)
+			fmt.Printf("%s | %s (%T)\n",
+				h.Gray(num), h.Code(t.String()), t)
+		}
+	} else {
+		fmt.Println("<no tokens>")
+	}
+	printLine()
+}
+
+func printLine() {
+	fmt.Printf("%s\n\n",
+		"====================")
+}
+
+func exit(context string, err error) {
+	fmt.Printf("%s: %s\n\n", h.Bold(context), err)
+	os.Exit(1)
+}
+
+func exite(context string, input []byte, err *e.Error) {
+	fmt.Printf("\n%s:\n\n%s\n\n", h.Bold(context), err.String(input))
+	os.Exit(1)
+}
