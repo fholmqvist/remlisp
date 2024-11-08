78e22eb2a59887d6d64b05dafcc1b3a9bd21ccfa
fholmqvist <fredrik.d.holmqvist@gmail.com>
2024-11-08 11:49:42 +0100

    skeleton for macro expansion

--- expander/expander.go
+++ expander/expander.go
@@ -45,18 +45,60 @@ func New(exprs []ex.Expr) *Expander {
 
 func (e *Expander) Expand() ([]ex.Expr, *e.Error) {
 	e.predeclareMacros()
-	for i, expr := range e.exprs {
-		expanded, err := e.expand(expr)
-		if err != nil {
+	for _, expr := range e.exprs {
+		if err := e.expand(expr); err != nil {
 			return nil, err
 		}
-		e.exprs[i] = expanded
 	}
 	return e.exprs, nil
 }
 
-func (e *Expander) expand(expr ex.Expr) (ex.Expr, *e.Error) {
-	return expr, nil
+func (e *Expander) expand(expr ex.Expr) *e.Error {
+	// TODO: Find all recursive calls.
+	switch expr := expr.(type) {
+	case *ex.List:
+		return e.expandCall(expr)
+	}
+	return nil
+}
+
+func (e *Expander) expandCall(list *ex.List) *e.Error {
+	if len(list.V) == 0 {
+		return nil
+	}
+	for _, expr := range list.V {
+		call, ok := expr.(ex.Identifier)
+		if !ok {
+			if list2, ok := expr.(*ex.List); ok {
+				if err := e.expandCall(list2); err != nil {
+					return err
+				}
+			}
+			continue
+		}
+		macro, ok := e.findMacro(call.V)
+		if !ok {
+			continue
+		}
+		if err := e.expandMacro(macro, list); err != nil {
+			return err
+		}
+		e.logMacroExpansion(call.V)
+	}
+	return nil
+}
+
+func (e *Expander) findMacro(name string) (*ex.Macro, bool) {
+	for _, m := range e.macros {
+		if m.Name == name {
+			return m, true
+		}
+	}
+	return nil, false
+}
+
+func (e *Expander) expandMacro(_ *ex.Macro, _ *ex.List) *e.Error {
+	return nil
 }
 
 func (e *Expander) predeclareMacros() {
@@ -71,6 +113,13 @@ func (e *Expander) predeclareMacros() {
 func (e *Expander) logMacro(m *ex.Macro) {
 	num := fmt.Sprintf("%.4d", e.printouts)
 	line := fmt.Sprintf("%s: %v", h.Bold("Read macro"), m.Name)
+	fmt.Printf("%s | %s\n", h.Gray(num), line)
+	e.printouts++
+}
+
+func (e *Expander) logMacroExpansion(name string) {
+	num := fmt.Sprintf("%.4d", e.printouts)
+	line := fmt.Sprintf("%s: %v", h.Bold("Expanded"), name)
 	fmt.Printf("%s | %s\n", h.Gray(num), line)
 	e.printouts++
 }
